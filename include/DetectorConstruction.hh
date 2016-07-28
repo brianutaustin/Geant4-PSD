0791160c77eb644da42f5d499ec9e96d772fee3d
dphan <brianp.dung@gmail.com>
2016-07-28 10:50:03 -0500

    Add new materials using G4NistManager. Target volume is defined again to be PbWO4.

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -40,6 +40,20 @@ private:
 		G4double TargetSizeX;
 		G4double TargetSizeY;
 		G4double TargetSizeZ;
+
+	// Materials
+	G4Material* C;
+	G4Material* Pb;
+	G4Material* Al;
+	G4Material* Cu;
+	G4Material* H2O;
+	G4Material* Sci;
+	G4Material* SiO2;
+	G4Material* Air;
+	G4Material* PbWO4;
+	G4Material* lAr;
+	G4Material* vac;
+	G4Material* coldAr;
 };
 
 #endif
